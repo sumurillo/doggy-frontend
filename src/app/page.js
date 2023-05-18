0c77fec51963188e9117a0d9b393bc942b51da40
sumurillo <sumire.ohana@gmail.com>
2023-05-18 10:18:40 -0500

    dogs api request working

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,28 @@
+'use client';
+
 import styles from './page.module.css'
 
+import { useState, useEffect } from 'react'
+
 export default function Home() {
+  const [dogs, setDogs] = useState([]);
+
+  useEffect(() => {
+    const getDogs = async () => {
+      const response = await fetch('https://dog-app-sumi.herokuapp.com/dogs');
+      const retrievedDogs = await response.json();
+      setDogs(retrievedDogs);
+    }
+
+    getDogs()
+
+  }, []);
   return (
     <main className={styles.main}>
       <h1>Hello React!</h1>
+      { dogs.map(dog => (
+        <h1>{ dog.name }</h1>
+      ))}
     </main>
   )
 }
